3cfffb3a20dd1034fc244354cd4ce944bcc22449
jennerdulce <jennerdulce@gmail.com>
2022-06-16 15:30:35 -0700

    added fuctionality for create route

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,24 @@ class StudentsController < ApplicationController
         student = Student.find(params[:id])
         render json: student
     end
+
+    def create
+        student = Student.create(student_params)
+        if student.valid?
+            render json: student
+        else
+            render json: student.errors
+        end
+    end
+
+    def destory
+        student = Student.find(params[:id])
+        student.destroy
+    end
+
+    private
+    # Strong parameter
+    def student_params
+        params.require(:student).permit(:name, :cohort)
+    end
 end
